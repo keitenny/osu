ae5828d3dc51f10308842ab8e608e0d9a7d3edb9
kei <kei_620@nifty.com>
2015-06-11 01:40:47 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Osu::Application.config.secret_key_base = '20151796b9def66809f49b2271d8eee5e0a83425a160250d425e2fabb8a71f7803db66ac7110b7815500adf6db9b18bb92d34389ae6a70c0013e0c26222d9dfc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Osu::Application.config.session_store :cookie_store, key: '_osu_session'
